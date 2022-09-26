f59d94234e4c8a289ebf8e5127e10c980a91e161
lgdhades <hades987456@outlook.com>
2022-09-26 23:35:28 +0200

    feat [Navbar] : add Navbar component

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import navbarLinks from '../../data/navbarLinks.json'
+import { Bell } from 'lucide-react'
+
+/*---> Component <---*/
+export const Navbar = () => {
+  const [activeLink, setActiveLink] = useState('')
+
+  return (
+    <NavbarWrapper>
+      <LogoWrapper>
+        <Logo src='/images/logo.png' alt='logo' />
+      </LogoWrapper>
+      <LinksWrapper>
+        {navbarLinks.map((link, index) => (
+          <Link
+            key={index}
+            link={link}
+            activeLink={activeLink}
+            onClick={() => setActiveLink(link)}
+          >
+            {link}
+          </Link>
+        ))}
+        <Bell />
+        <Number>4</Number>
+        <Avatar src='/images/avatar2.png' alt='avatar' />
+      </LinksWrapper>
+    </NavbarWrapper>
+  )
+}
+
+/*---> Styles <---*/
+const NavbarWrapper = styled.div`
+  /* border: 1px solid yellow; */
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`
+
+const LogoWrapper = styled.div`
+  /* border: 1px solid red; */
+`
+const Logo = styled.img`
+  /* border: 1px solid red; */
+  width: 140px;
+  object-fit: cover;
+  object-position: center;
+`
+
+const LinksWrapper = styled.div`
+  /* border: 1px solid red; */
+  display: flex;
+  align-items: center;
+`
+
+const Link = styled.div`
+  /* border: 1px solid black; */
+  margin-right: 10px;
+  padding: 5px 0px;
+  border-bottom: ${(props) =>
+    props.activeLink === props.link ? '2px solid #ff0000' : 'none'};
+  cursor: pointer;
+
+  :hover {
+    border-bottom: 2px solid #ff0000;
+  }
+`
+
+const Number = styled.div`
+  border: 1px solid red;
+  border-radius: 50%;
+  background: #ff0000;
+  color: white;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 400;
+  font-size: 12px;
+  margin-bottom: 30px;
+  margin-right: 10px;
+`
+
+const Avatar = styled.img`
+  /* border: 1px solid red; */
+  object-fit: cover;
+  object-position: center;
+`
